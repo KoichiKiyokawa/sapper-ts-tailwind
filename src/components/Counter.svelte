2a5b96dc4602aba1f9933f198cb2e4e2c08a3b20
K.Kiyokawa <koichi20110068@gmail.com>
2020-09-05 17:12:22 +0000

    feat: add counter

---
+++ src/components/Counter.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+  import { createEventDispatcher } from "svelte";
+  const dispatch = createEventDispatcher();
+
+  export let count: number;
+  export let text: string;
+</script>
+
+<div>
+  <input value={text}>
+  <span>{count}</span>
+  <button on:click={() => { dispatch("increment") }}>+1</button>
+  <button on:click={() => { dispatch("decrement")}}>-1</button>
+  <button on:click={() => { dispatch("drop")}}>x</button>
+</div>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,12 +1,47 @@
 <script lang="ts">
-  let msg: string = "hoge";
+  import Counter from "../components/Counter.svelte";
+
+  const getDefaultCounter = () => ({
+    count: 0,
+    text: "new",
+  });
+  let counters = [getDefaultCounter()];
+
+  const handler = (index: number) => ({
+    increment() {
+      counters[index].count++;
+    },
+    decrement() {
+      if (counters[index].count > 0) counters[index].count--;
+    },
+    drop() {
+      counters = counters.filter((_, i) => i !== index)
+    }
+  })
+
+  const addCounter = () => {
+    counters = [...counters, getDefaultCounter()];
+  };
 </script>
 
 <svelte:head>
   <title>Sapper project template</title>
 </svelte:head>
 
-<template lang="pug">
-  h1 msg
-  h2 {msg}
-</template>
+<!-- <template lang="pug">
+  +each('counters as count')
+  Counter(count!="{count}") // error count is not defined pugとtsの相性悪い。。。
+</template> -->
+
+{#each counters as counter, i}
+  <Counter
+    count={counter.count}
+    text={counter.text}
+    on:increment={handler(i).increment}
+    on:decrement={handler(i).decrement}
+    on:drop={handler(i).drop} />
+{/each}
+{#if counters.length === 0}
+<p>Push "Add Counter" to make counter.</p>
+{/if}
+<div><button on:click={addCounter}>Add Counter</button></div>
