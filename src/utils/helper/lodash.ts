082b6521f7b926c20d84cdc7f49c4e9b7c37a25f
K.Kiyokawa <koichi20110068@gmail.com>
2020-09-19 00:42:32 +0900

    feat: add compose method

--- src/utils/helper/lodash.ts
+++ src/utils/helper/lodash.ts
@@ -1,3 +1,11 @@
 export const sum = (nums: number[]) => nums.reduce((a, c) => a + c, 0);
 export const sumBy = (nums: object[], key: string) =>
   nums.reduce((a, c) => a + (c[key] as number), 0);
+/**
+ * @example
+ * compose(add1, double)(1)
+ * // => double(add1(1))
+ * // => double(2)
+ * // => 4
+ */
+export const compose = (...functions: Function[]) => (arg: any) => functions.reduce((a, fn) => fn(a), arg)
