5c60fd4168eb179f66297c0338748af86b6f19e1
K.Kiyokawa <koichi20110068@gmail.com>
2020-09-19 00:42:41 +0900

    feat: add notification component

---
+++ src/components/Notify.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+  export let open = false
+  export let title = 'Title'
+  export let body = 'this is a notify body.'
+</script>
+
+<div class="notification">
+  <div class="title">{title}</div>
+  <div class="body">{body}</div>
+</div>
+
+<style lang="sass">
+  
+</style>
